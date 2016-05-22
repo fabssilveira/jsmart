4edf8a5c444a8c6e9218ecf6190e42be38008045
Fabio Silveira <fabssilveira@gygatech.com.br>
2016-05-22 14:00:01 -0300

    driver jdbc corrigido

--- src/br/com/jsmartmarket/jpa/controller/JsmartController.java
+++ src/br/com/jsmartmarket/jpa/controller/JsmartController.java
@@ -3,11 +3,23 @@ package br.com.jsmartmarket.jpa.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import br.com.jsmartmarket.jpa.dao.AutorizaDao;
+import br.com.jsmartmarket.jpa.dao.ClienteDao;
+import br.com.jsmartmarket.jpa.dao.EnderecoDao;
+import br.com.jsmartmarket.jpa.model.Autoriza;
+import br.com.jsmartmarket.jpa.model.Cliente;
+import br.com.jsmartmarket.jpa.model.Endereco;
+
 @Controller
 public class JsmartController {
 
 	@RequestMapping("/gravaCliente")
-	public String formulario(){
+	public String formulario(Endereco endereco, Cliente cliente, Autoriza autoriza){
+		
+		new EnderecoDao().salvar(endereco);
+		new ClienteDao().salvar(cliente);
+		new AutorizaDao().salvar(autoriza);
+		
 		return "formulario";
 	}
 
