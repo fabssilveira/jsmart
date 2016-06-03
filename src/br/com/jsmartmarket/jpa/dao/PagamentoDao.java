e7d8fb4a6b6bd360502e616e0f408681010ae07c
fabio <fabssilveira@gygatech.com.br>
2016-06-03 15:07:31 -0300

    Daos

--- src/br/com/jsmartmarket/jpa/dao/PagamentoDao.java
+++ src/br/com/jsmartmarket/jpa/dao/PagamentoDao.java
@@ -1,9 +1,26 @@
 package br.com.jsmartmarket.jpa.dao;
 
+import java.util.List;
+
+import javax.persistence.NoResultException;
+import javax.persistence.Query;
+
 import br.com.jsmartmarket.jpa.model.Pagamento;
 
 public class PagamentoDao extends DaoGenerico<Long, Pagamento>{
 	
-	
+	@SuppressWarnings("unchecked")
+	public Pagamento buscaPagamento(int codigoPagamento){
+		
+		Query qr = em.createQuery("select p from Pagamento as p "+
+          "where p.codigoPagamento = :codigoPagamento");
+		qr.setParameter("codigoPagamento", codigoPagamento);
+		
+		try{
+			return (Pagamento) qr.getSingleResult();
+		}catch(NoResultException e){
+			return null;
+		}
+	}
 
 }
